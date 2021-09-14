0de2f52abcd29d2fe39c2a958dd24e02946209b3
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-14 14:11:07 +0200

    Created main.py

---
+++ Python-Wallet/main.py
@@ -0,0 +1,56 @@
+import hashlib
+import random
+import socket
+import threading
+import time
+
+class main :
+    
+    class datas :
+        
+        addressesBytesSize = 64
+        blockTime = 30
+        blockMaxTransactions = 10000000
+        
+        blocksNotValited = []
+        blockTransactions = []
+        transctionsUnspent = []
+        
+        privateAddresses = []
+        publicAddresses = []
+        
+    class blockchain :
+        
+        def createBlock() :
+            
+            
+            
+        def createTransaction() :
+            
+            
+            
+    class wallet :
+        
+        def createPrivateAddress() :
+            
+            i = 0
+            self.privkey = None
+            self.privatekey = 0000000000000000000000000000000000000000000000000000000000000000
+            for self.privkey /8 > datas.addressesBytesSize :
+                
+                
+                
+            else :
+                
+                for i > datas.addressesBytesSize :
+                    
+                    
+                    i += 1
+                    
+                else :
+                    
+                    datas.privateAddresses.append("'", self.privatekey, "'")
+                    
+        def createPublicAddress() :
+            
+            random.randint(0, 1)
